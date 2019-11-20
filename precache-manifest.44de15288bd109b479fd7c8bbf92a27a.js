f08033bee8b0929566a11ba1f3c1d86df6bd31bf
berniecultess <berniecultess@gmail.com>
2019-11-20 12:40:25 -0500

    Updates

---
+++ precache-manifest.44de15288bd109b479fd7c8bbf92a27a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb4c279b8bfb1eb114e35f8259fe60f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1ddfe23846b428280b5",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "f6f29d1d54a766a9c34b",
+    "url": "/static/js/2.30484312.chunk.js"
+  },
+  {
+    "revision": "c1ddfe23846b428280b5",
+    "url": "/static/js/main.0c0782e9.chunk.js"
+  },
+  {
+    "revision": "602d592b55e89a37b450",
+    "url": "/static/js/runtime-main.a3630d87.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
