08a0cf2b5687b772a2d0b2fd98fdd4054d3b1299
Bernie Cultess <cultessdesign@gmail.com>
2019-12-01 20:21:41 -0500

    Additional updates
    created skills.json; 
    placed components within it's own folder;

---
+++ precache-manifest.2953468b747342a69da1f067019592c0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef9320cfa331c7812b538472b4e1485c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ac73cedf460722041cf",
+    "url": "/static/css/main.f334250a.chunk.css"
+  },
+  {
+    "revision": "a89fcb58f5887d79657d",
+    "url": "/static/js/2.d106909f.chunk.js"
+  },
+  {
+    "revision": "7ac73cedf460722041cf",
+    "url": "/static/js/main.47165f84.chunk.js"
+  },
+  {
+    "revision": "602d592b55e89a37b450",
+    "url": "/static/js/runtime-main.a3630d87.js"
+  },
+  {
+    "revision": "3c00e0f8f65775e9f0464ae33b2b4522",
+    "url": "/static/media/Bernard-Cultess-Resume-2019.3c00e0f8.pdf"
+  },
+  {
+    "revision": "e5fa07bb3a53280056c6df55e98ff44d",
+    "url": "/static/media/bg-1.e5fa07bb.png"
+  },
+  {
+    "revision": "2e24f873d8835c8342d57f1b7e63d7b1",
+    "url": "/static/media/bg-2.2e24f873.png"
+  },
+  {
+    "revision": "b62711c8077037a05ef6a7c743d9b7da",
+    "url": "/static/media/bg-3.b62711c8.png"
+  },
+  {
+    "revision": "10e20c628ca3077715d9353561c5acc2",
+    "url": "/static/media/bg-hireme-Img.10e20c62.jpg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2e06b8a687b0efb0d6f5919f35ad2079.js"
+  "/precache-manifest.2953468b747342a69da1f067019592c0.js"
 );
 
 self.addEventListener('message', (event) => {
